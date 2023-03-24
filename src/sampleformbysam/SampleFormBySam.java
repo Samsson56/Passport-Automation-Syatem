f15a46f5519843ebb3171f37f2728398365407b4
Samsson R <91111124+Samsson56@users.noreply.github.com>
2023-03-24 15:50:16 +0530

    Add files via upload

---
+++ src/sampleformbysam/SampleFormBySam.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sampleformbysam;
+
+/**
+ *
+ * @author DSLAB-30
+ */
+public class SampleFormBySam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
